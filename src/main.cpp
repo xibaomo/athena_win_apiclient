cb0e531cf5fb0d900d1947f3f995c00cfa2b9963
fxua <fxua@gmail.com>
2018-11-17 02:05:17 -0800

    api client and server work!!!!

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,22 @@ using namespace std;
 int main(int argc, char** argv)
 {
 
-    wchar_t* hostip = L"192.168.1.102";
-    wchar_t* port = L"8800";
+    wchar_t* hostip = L"73.92.253.8";
+    wchar_t* port = L"8888";
     wchar_t* symbol = L"EURUSD";
 
 //    test_api_server(hostip,port);
 
-    float a = 1.66666;
-
     athena_init(symbol,hostip,port);
+
+    Real p[500];
+    for (int i=0;i<500;i++)
+        p[i] = 1.0;
+    sendHistoryTicks(p,500,L"buy");
+
+    float pc = 1.666;
+    int action = classifyATick(pc,L"buy");
+
+    printf("action %d\n",action);
     return 0;
 }
