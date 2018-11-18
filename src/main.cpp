62f99b24bac94bde1fdaffa3f85cf163a5006d07
fxua <fxua@gmail.com>
2018-11-18 02:33:21 -0800

    able to call api under release build

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ int main(int argc, char** argv)
 
     athena_init(symbol,hostip,port);
 
-//    Real p[500];
-//    for (int i=0;i<500;i++)
-//        p[i] = 1.0;
-//    sendHistoryTicks(p,500,L"buy");
+    Real p[500];
+    for (int i=0;i<500;i++)
+        p[i] = 1.0;
+    sendHistoryTicks(p,500,L"buy");
 
     float pc = 1.666;
     int action=-1;
-    for (int i=0;i<510;i++) {
+    for (int i=0;i<10;i++) {
         action = classifyATick(pc,L"buy");
         printf("action %d\n",action);
     }
