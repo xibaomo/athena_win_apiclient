07a7c4897444522b50bfb2c1fc5d0f7e0948515a
fxua <fxua@gmail.com>
2018-12-13 02:29:46 -0800

    use true file instead of symlink

--- src/basics/log.cpp
+++ src/basics/log.cpp
@@ -1,1 +1,28 @@
-../../../athena/api/api_athena/src/common/basics/log.cpp
+/*
+ * =====================================================================================
+ *
+ *       Filename:  log.cpp
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  10/27/2018 12:26:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#include "log.h"
+#define BOOST_LOG_DYN_LINK 1
+//Logger Log = Logger::getInstance();
+
+//LogLevel LOG_FATAL = LogLevel::LOG_INFO;
+//LogLevel LOG_ERROR = LogLevel::LOG_ERROR;
+//LogLevel LOG_WARNING = LogLevel::LOG_WARNING;
+//LogLevel LOG_INFO = LogLevel::LOG_INFO;
+//LogLevel LOG_DEBUG = LogLevel::LOG_DEBUG;
+//LogLevel LOG_VERBOSE = LogLevel::LOG_VERBOSE;
--- src/basics/types.h
+++ src/basics/types.h
@@ -1,1 +1,38 @@
-../../../athena/api/api_athena/src/common/basics/types.h
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  10/27/2018 12:48:35 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#ifndef  _BASICS_TYPES_H_
+#define  _BASICS_TYPES_H_
+#include <string>
+
+#define ONE_MS 1
+#define ONE_HUNDRED_MS 100
+
+typedef unsigned int Uint;
+typedef unsigned char Uchar;
+using String = std::string;
+
+#ifdef __USE_64_BIT
+typedef double Real;
+#define REALFORMAT "d"
+#else
+#define __USE_32_BIT
+typedef float Real;
+#define REALFORMAT "f"
+#endif
+#endif   /* ----- #ifndef _BASICS_TYPES_H_  ----- */
